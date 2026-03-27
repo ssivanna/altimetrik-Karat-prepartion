40bb0e6ab73229328ae8df62159668be6554d89a
ssivanna <120359678+ssivanna@users.noreply.github.com>
2026-03-27 18:59:24 +0530

    Add files via upload

---
+++ Revrsestring.java
@@ -0,0 +1,39 @@
+
+public class Revrsestring {
+	
+	
+	public static void main(String args[])
+	{
+		//System.out.println("hello");
+		String str="welcome";
+		String rvs=" ";
+		/*
+		 * //System.out.println(str.charAt(3)+ "-> "+ str.length()); for( int
+		 * i=str.length()-1;i>=0;i--) { //System.out.println(str.charAt(i));
+		 * rvs=rvs+str.charAt(i);
+		 * 
+		 * 
+		 * }
+		 */
+		
+		/*
+		 * char ch[]=str.toCharArray();
+		 * 
+		 * for(int i=ch.length-1;i>=0;i--) {
+		 * 
+		 * rvs=rvs+ch[i];
+		 * 
+		 * }
+		 * 
+		 * System.out.println(rvs);
+		 */
+		
+		String str1="sivanna";
+		         
+		System.out.println(str1.concat("kumar"));
+		
+		
+		 
+	}
+
+}
